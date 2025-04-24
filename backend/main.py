212e0b0205d306642bd153da7538ca5209bb88d5
kirill <kuil-2.0@ya.ru>
2025-04-24 11:49:48 +0300

    fix CORS

--- backend/main.py
+++ backend/main.py
@@ -10,12 +10,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 
 app.include_router(api_router)
